e9207d953ab1c93b0bccdc7e02f1c54a4750bf44
hossam43 <hossamayman876.gmail.com>
2024-02-08 19:41:08 +0200

    add source

--- src/auth.js
+++ src/auth.js
@@ -1,4 +1,6 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, doc } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCneS2eJYBRYy-lD4rTKw4VKqtWGoVnfto",
   authDomain: "asl-academy.firebaseapp.com",
@@ -8,3 +10,28 @@ const firebaseConfig = {
   appId: "1:814578248642:web:debc7e17bfb4fd5a60045b",
   measurementId: "G-X7077R1T8G",
 };
+
+// init firebase app
+initializeApp(firebaseConfig);
+
+//  init services
+const db = getFirestore();
+
+// collection ref
+const colRef = collection(db, "users");
+
+//  get collection data
+getDocs(colRef)
+  .then((snapshot) => {
+    let users = [];
+    snapshot.docs.forEach((doc) => {
+      users.push({ ...doc.data(), id: doc.id });
+    });
+    console.log(users);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const loginForm = document.querySelector(".login");
+loginForm.addEventListener("click", () => {});
