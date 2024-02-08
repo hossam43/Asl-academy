0c298d1c5ab112bfaac1a858ce17290947c42dea
hossam43 <hossamayman876.gmail.com>
2024-02-09 01:38:34 +0200

    update login

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, doc } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  addDoc,
+} from "firebase/firestore";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signOut,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCneS2eJYBRYy-lD4rTKw4VKqtWGoVnfto",
@@ -16,22 +27,79 @@ initializeApp(firebaseConfig);
 
 //  init services
 const db = getFirestore();
-
+const auth = getAuth();
 // collection ref
 const colRef = collection(db, "users");
 
-//  get collection data
-getDocs(colRef)
-  .then((snapshot) => {
-    let users = [];
-    snapshot.docs.forEach((doc) => {
-      users.push({ ...doc.data(), id: doc.id });
-    });
-    console.log(users);
-  })
-  .catch((err) => {
-    console.log(err.message);
+//  realtime collection data
+onSnapshot(colRef, (snapshot) => {
+  let users = [];
+  snapshot.docs.forEach((doc) => {
+    users.push({ ...doc.data(), id: doc.id });
   });
+  console.log(users);
+});
 
+const registerForm = document.querySelector(".register");
+if (registerForm) {
+  registerForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const fullName = registerForm.full_name.value;
+    const email = registerForm.email.value;
+    const password = registerForm.password.value;
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+
+        // Add user data to Firestore
+        return addDoc(collection(db, "users"), {
+          uid: user.uid,
+          email: user.email,
+          fullName: fullName,
+        });
+      })
+      .then(() => {
+        registerForm.reset();
+        // Redirect the user to index.html
+        window.location.href = "index.html";
+        console.log("User created successfully 🟢");
+      })
+      .catch((err) => console.log(err.message));
+  });
+}
+
+const logOutBtn = document.querySelector(".logout");
+if (logOutBtn) {
+  logOutBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    signOut(auth)
+      .then(() => {
+        // Redirect the user to register.html after logout
+        window.location.href = "register.html";
+        console.log("User logged out");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
+}
+
+// login.html
 const loginForm = document.querySelector(".login");
-loginForm.addEventListener("click", () => {});
+
+if (loginForm) {
+  loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = loginForm.email.value;
+    const password = loginForm.password.value;
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        loginForm.reset();
+        // Redirect the user to index.html
+        window.location.href = "index.html";
+        console.log("User logged in successfully 🟢");
+      })
+      .catch((err) => console.log(err.message));
+  });
+}
